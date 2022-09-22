85728051ce3530ed74541ef12f2d336683b6a3ba
Misha <landofcoder@gmail.com>
2022-09-22 22:33:35 +0700

    fix issue for magento 2.4.x

--- Block/Adminhtml/RetailerOffer/ProductPicker.php
+++ Block/Adminhtml/RetailerOffer/ProductPicker.php
@@ -49,7 +49,8 @@ class ProductPicker extends \Magento\Backend\Block\AbstractBlock
      */
     protected function _toHtml()
     {
-        return $this->escapeJsQuote($this->getForm()->toHtml());
+        return $this->getForm()->toHtml();
+        //return $this->escapeJsQuote($this->getForm()->toHtml());
     }
 
     /**
